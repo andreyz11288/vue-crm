9afb81a556ceb61959ccf5e78cb7a62687a784a0
Andrii <andriiz@default-value.com>
2024-01-03 20:16:08 +0200

    Сделал попап rules

---
+++ src/components/Rules.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <i class="medium material-icons button-position" @click.prevent="hidePopup">close</i>
+    <div class="card rules-card">
+      <span>
+        Welcome to our CRM application for planning and accounting of costs and income with convenient division into categories! Our application is created taking into account your needs for effective financial management and budget management.
+        <br><br>
+        <strong>Main functionality:</strong>
+        <br><br>
+        <strong>1) Budget Planning:</strong><br>
+        Set cost and revenue goals and monitor plan execution. Our app will help you better understand where your finances are going.
+        <br>
+        <strong>2) Categories of Costs and Income:</strong><br>
+        Break down your expenses and income into manageable categories to get a detailed picture of your financial health.
+        <br>
+        <strong>3) User-friendly Interface:</strong><br>
+        A simple and intuitive interface makes it easy to use. You can quickly add and track transactions.
+        <br>
+        <strong>4) Reports and Analytics:</strong><br>
+        Get detailed reports and analytics on your finances to help you make more informed decisions and improve your financial planning.
+        <br>
+        <strong>5) Security and Privacy:</strong><br>
+        We value your privacy. Your financial data is kept secure, ensuring your personal information is protected.
+        <br><br>
+        <strong>How to start:</strong><br>
+        <br>
+        <strong>1) Register:</strong><br>
+        Create an account to start using all the features of our CRM.
+        <br>
+        <strong>2) Add Transactions:</strong><br>
+        Regularly add your financial transactions by specifying cost and income categories.
+        <br>
+        <strong>3) Analyze and Plan:</strong><br>
+        View reports, analyze spending patterns, and plan your finances for the future.
+        <br>
+        We are confident that our CRM application will make accounting and management of your finances simpler and more efficient. Get started now!
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    hidePopup() {
+      this.$emit('hide', {
+        showPopup: false
+      })
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.button-position {
+  position: absolute;
+  z-index: 2;
+  top: 15px;
+  right: 45px;
+  cursor: pointer;
+}
+.rules-card {
+  position: absolute;
+  top: 70px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 700px;
+  height: 700px;
+  z-index: 1;
+  padding: 50px 50px;
+  overflow: auto;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,105 +1,110 @@
-<script setup>
-
-</script>
-
 <template>
-  <form class="card auth-card" @submit.prevent="submitHandler">
-    <div class="card-content">
-      <span class="card-title">{{$filters.localize.localizeFilter('Home accounting')}}</span>
-      <div class="input-field">
-        <input
-            id="email"
-            type="text"
-            v-model.trim="email"
-        :class="{invalid: (v$.email.$dirty && v$.email.$invalid) || (v$.email.$dirty && v$.email.required.$invalid)}"
-        >
-        <label for="email">Email</label>
-        <small
-            class="helper-text invalid"
-            v-if="v$.email.$dirty && v$.email.required.$invalid"
-        >{{$filters.localize.localizeFilter('The Email field must not be empty')}}</small>
-        <small
-            class="helper-text invalid"
-            v-else-if="v$.email.$dirty && v$.email.$invalid"
-        >{{$filters.localize.localizeFilter('Enter correct Email')}}</small>
+  <div>
+    <form class="card auth-card" @submit.prevent="submitHandler">
+      <div class="card-content">
+        <span class="card-title">{{$filters.localize.localizeFilter('Home accounting')}}</span>
+        <div class="input-field">
+          <input
+              id="email"
+              type="text"
+              v-model.trim="email"
+          :class="{invalid: (v$.email.$dirty && v$.email.$invalid) || (v$.email.$dirty && v$.email.required.$invalid)}"
+          >
+          <label for="email">Email</label>
+          <small
+              class="helper-text invalid"
+              v-if="v$.email.$dirty && v$.email.required.$invalid"
+          >{{$filters.localize.localizeFilter('The Email field must not be empty')}}</small>
+          <small
+              class="helper-text invalid"
+              v-else-if="v$.email.$dirty && v$.email.$invalid"
+          >{{$filters.localize.localizeFilter('Enter correct Email')}}</small>
+        </div>
+        <div class="input-field">
+          <input
+              id="password"
+              type="password"
+              v-model.trim="password"
+              :class="{invalid: (v$.password.$dirty && v$.password.minLength.$invalid) || (v$.password.$dirty && v$.password.required.$invalid)}"
+          >
+          <label for="password">{{$filters.localize.localizeFilter('Password')}}</label>
+          <small
+              class="helper-text invalid"
+              v-if="v$.password.$dirty && v$.password.required.$invalid"
+          >
+            {{$filters.localize.localizeFilter('Enter password')}}
+          </small>
+          <small
+              class="helper-text invalid"
+              v-else-if="v$.password.$dirty && v$.password.minLength.$invalid"
+          >
+            {{$filters.localize.localizeFilter('The password must be')}} {{v$.password.minLength.$params.min}} {{$filters.localize.localizeFilter('characters. Now he')}} {{password.length}}
+          </small>
+        </div>
+        <div class="input-field">
+          <input
+              id="name"
+              type="text"
+              v-model.trim="name"
+              :class="{invalid: (v$.name.$dirty && v$.name.minLength.$invalid) || (v$.name.$dirty && v$.name.required.$invalid)}"
+          >
+          <label for="name">{{$filters.localize.localizeFilter('Name')}}</label>
+          <small
+              class="helper-text invalid"
+              v-if="v$.name.$dirty && v$.name.required.$invalid"
+          >
+            {{$filters.localize.localizeFilter('Enter your name')}}
+          </small>
+          <small
+              class="helper-text invalid"
+              v-else-if="v$.name.$dirty && v$.name.minLength.$invalid"
+          >
+            {{$filters.localize.localizeFilter('The name must be')}} {{v$.name.minLength.$params.min}} {{$filters.localize.localizeFilter('characters. Now it is')}} {{name.length}}
+          </small>
+        </div>
+        <p>
+          <label>
+            <input type="checkbox" v-model="agree" />
+            <span>{{$filters.localize.localizeFilter('I agree with the')}}</span>
+          </label>
+          <span class="rules" @click="() => {handelRules = !handelRules}">{{$filters.localize.localizeFilter('rules')}}</span>
+        </p>
       </div>
-      <div class="input-field">
-        <input
-            id="password"
-            type="password"
-            v-model.trim="password"
-            :class="{invalid: (v$.password.$dirty && v$.password.minLength.$invalid) || (v$.password.$dirty && v$.password.required.$invalid)}"
-        >
-        <label for="password">{{$filters.localize.localizeFilter('Password')}}</label>
-        <small
-            class="helper-text invalid"
-            v-if="v$.password.$dirty && v$.password.required.$invalid"
-        >
-          {{$filters.localize.localizeFilter('Enter password')}}
-        </small>
-        <small
-            class="helper-text invalid"
-            v-else-if="v$.password.$dirty && v$.password.minLength.$invalid"
-        >
-          {{$filters.localize.localizeFilter('The password must be')}} {{v$.password.minLength.$params.min}} {{$filters.localize.localizeFilter('characters. Now he')}} {{password.length}}
-        </small>
-      </div>
-      <div class="input-field">
-        <input
-            id="name"
-            type="text"
-            v-model.trim="name"
-            :class="{invalid: (v$.name.$dirty && v$.name.minLength.$invalid) || (v$.name.$dirty && v$.name.required.$invalid)}"
-        >
-        <label for="name">{{$filters.localize.localizeFilter('Name')}}</label>
-        <small
-            class="helper-text invalid"
-            v-if="v$.name.$dirty && v$.name.required.$invalid"
-        >
-          {{$filters.localize.localizeFilter('Enter your name')}}
-        </small>
-        <small
-            class="helper-text invalid"
-            v-else-if="v$.name.$dirty && v$.name.minLength.$invalid"
-        >
-          {{$filters.localize.localizeFilter('The name must be')}} {{v$.name.minLength.$params.min}} {{$filters.localize.localizeFilter('characters. Now it is')}} {{name.length}}
-        </small>
-      </div>
-      <p>
-        <label>
-          <input type="checkbox" v-model="agree"/>
-          <span>{{$filters.localize.localizeFilter('I agree with the rules')}}</span>
-        </label>
-      </p>
-    </div>
-    <div class="card-action">
-      <div>
-        <button
-            class="btn waves-effect waves-light auth-submit"
-            type="submit"
-        >
-          {{$filters.localize.localizeFilter('Register')}}
-          <i class="material-icons right">send</i>
-        </button>
-      </div>
+      <div class="card-action">
+        <div>
+          <button
+              class="btn waves-effect waves-light auth-submit"
+              type="submit"
+          >
+            {{$filters.localize.localizeFilter('Register')}}
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
 
-      <p class="center">
-        {{$filters.localize.localizeFilter('Already have an account?')}}
-        <router-link to="/login">{{$filters.localize.localizeFilter('Login')}}!</router-link>
-      </p>
-    </div>
-  </form>
+        <p class="center">
+          {{$filters.localize.localizeFilter('Already have an account?')}}
+          <router-link to="/login">{{$filters.localize.localizeFilter('Login')}}!</router-link>
+        </p>
+      </div>
+    </form>
+
+    <Rules v-if="handelRules" @hide="showPopup"/>
+
+  </div>
 </template>
 <script>
 import { useVuelidate } from '@vuelidate/core'
 import {email, required, minLength} from '@vuelidate/validators'
+import Rules from "@/components/Rules.vue"
 export default {
+  components: {Rules},
   setup: () => ({ v$: useVuelidate() }),
   data: () => ({
     email: '',
     password: '',
     name:'',
     agree: false,
+    handelRules: false,
   }),
   validations () {
     return {
@@ -109,7 +114,16 @@ export default {
       agree: {checked: v => v}
     }
   },
+  mounted() {
+
+    },
   methods: {
+    showPopup(e) {
+      if (e) {
+        this.handelRules = e.showPopup
+      }
+      console.log(e.showPopup)
+    },
     async submitHandler() {
       if (this.v$.$invalid) {
         this.v$.$touch()
@@ -129,5 +143,9 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-
+.rules {
+  cursor: pointer;
+  margin-left: 5px;
+  text-decoration: underline;
+}
 </style>
